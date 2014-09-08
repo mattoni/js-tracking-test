b8e7a0fa8a2089bdb3c228b1333a960b2e6f8c13
AlexanderMattoni <amattoni@petrichor.io>
2014-09-08 00:33:20 -0400

    only after page closed

--- public_html/rand/getmousecoords.php
+++ public_html/rand/getmousecoords.php
@@ -29,7 +29,7 @@
 			console.log(data);
 			xhr = new XMLHttpRequest;
 			xhr.open('POST', 'logcoords.php');
-			xhr.setRequestHeader("Content-Type", "application/json");
+			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 			xhr.send('data='+data);
 			xhr.onreadystatechange=function() {
 				console.log(xhr.responseText);
