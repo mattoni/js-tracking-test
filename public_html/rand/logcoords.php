375a3a4c9674daaaf625e90b14ec68be6068bc09
AlexanderMattoni <amattoni@petrichor.io>
2014-09-08 00:09:52 -0400

    async test

--- public_html/rand/logcoords.php
+++ public_html/rand/logcoords.php
@@ -1,6 +1,5 @@
 <?php
 
+file_put_contents('coords.json', $_POST['coords']);
 
-file_put_contents('coords.json', $_POST);
-
-exit("Stored coordinates X: {$_POST['xcoord']}, Y: {$_POST['ycoord']}");
+exit("Stored coordinates...");
