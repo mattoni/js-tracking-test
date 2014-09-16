9a24b4ee5b1317d27c561a1c0c12a05ec59be366
AlexanderMattoni <amattoni@petrichor.io>
2014-09-16 17:16:14 -0400

    sandbox iframe

--- public_html/rand/testsessionplayback.php
+++ public_html/rand/testsessionplayback.php
@@ -6,25 +6,23 @@
 <body>
 	<iframe sandbox="allow-same-origin" src="http://alexmattoni.com/rand/getmousecoords.php" width="80%" height="600" id='frameDemo'></iframe>
 	<script>
-		var data = {};
-		$.getJSON("sample_session.json", function(result) {
-			console.log(result);
-			data = result;
-		});
 		var counts = {
 			clicks : {}
 		};
+
+		$.getJSON("sample_session.json", function(result) {
+			$.each(result.timeline.clicks, function(index, element) {
+				var path = element.path.replace(/\s+/g, '');
+				if(path in counts.clicks) {
+					counts.clicks[path] ++;
+				} else {
+					counts.clicks[path] = 1;
+				}
+			});
+		});
 		//remove javascript in iframe
 		$("#frameDemo").contents().find('script').remove();
 
-		$.each(data.timeline.clicks, function(index, element) {
-			var path = element.path.replace(/\s+/g, '');
-			if(path in counts.clicks) {
-				counts.clicks[path] ++;
-			} else {
-				counts.clicks[path] = 1;
-			}
-		});
 		$("#frameDemo").load(function() {
 			$.each(data.timeline.clicks, function(index, element) {
 				$("#frameDemo").contents().find(element.path).append(' (' + counts.clicks[element.path.replace(/\s+/g, '')] + ' clicks)');//.css("color","#BADA55");
