1c307dd8f575eff5b644f2ef543a1aebdf1f879d
AlexanderMattoni <amattoni@petrichor.io>
2014-09-16 11:49:59 -0400

    test

--- public_html/rand/captureafo.php
+++ public_html/rand/captureafo.php
@@ -1,14 +1,15 @@
 <?php
 
 $dir = '/home/amattoni/public_html/rand/images/';
+$command = 'pageres [ https://afo.com 1280x1024 ]';
 
 rmdir($dir);
 mkdir($dir);
 chdir($dir);
-exec('pageres [ https://afo.com 1280x1024 ] ', $output, $response);
+exec('pageres https://afo.com [1280x1024 ] ', $output, $response);
 
 print_r($output);
 
-echo "Images located <a href='/rand/images/'>here</a>";
+echo "<br /> <br />Images located <a href='/rand/images/'>here</a>";
 
 exit();
