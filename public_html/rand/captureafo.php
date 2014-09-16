d9879cd740a3311112ac8e87825c7a96d6a70807
AlexanderMattoni <amattoni@petrichor.io>
2014-09-16 12:01:54 -0400

    test

--- public_html/rand/captureafo.php
+++ public_html/rand/captureafo.php
@@ -3,6 +3,7 @@
 exec('phantomjs /home/amattoni/public_html/rand/phantomjs/capture.js', $output, $response);
 
 print_r($output);
+print_r($response);
 
 echo "<br /> <br /><img src='/rand/afo.png'>";
 
