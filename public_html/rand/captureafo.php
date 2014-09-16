81dab7c9342e4f93832c50784749c6fb2c2f20fb
AlexanderMattoni <amattoni@petrichor.io>
2014-09-16 01:33:56 -0400

    test

--- public_html/rand/captureafo.php
+++ public_html/rand/captureafo.php
@@ -1,14 +1,16 @@
 <?php
 
 $url = 'https://afo.com';
+$dir = '/home/amattoni/public_html/rand/images/';
 $resolutions = array(
 	'1600x900',
 	'1920x1080',
 	'1372x1843'
 );
 
-mkdir('/home/amattoni/public_html/rand/images/');
-chdir('/home/amattoni/public_html/rand/images');
+rmdir($dir);
+mkdir($dir);
+chdir($dir);
 exec('pageres ' . $url .' 1920x1080', $output, $response);
 
 print_r($output);
