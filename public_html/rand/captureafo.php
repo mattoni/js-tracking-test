ae32be3b22d861ada75bdcfe061689b456b1fa04
AlexanderMattoni <amattoni@petrichor.io>
2014-09-16 01:23:40 -0400

    test

--- public_html/rand/captureafo.php
+++ public_html/rand/captureafo.php
@@ -7,11 +7,12 @@ $resolutions = array(
 	'1372x1843'
 );
 
+mkdir('/home/amattoni/public_html/rand/images/');
 exec('cd /home/amattoni/public_html/rand/images/');
+echo 'pageres ' . $url .' ' . implode(' ', $resolutions);
+//exec('pageres ' . $url .' ' . implode(' ', $resolutions), $output, $response);
 
-exec('pageres ' . $url .' ' . implode(' ', $resolutions), $output, $response);
-
-echo 'Completed with message: ' . $response;
+echo 'Completed with message: ' ;//. $response;
 
 echo "Images located <a href='/rand/images/'>here</a>";
 
