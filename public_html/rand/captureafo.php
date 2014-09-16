7bcca3a27d8ee17f73df54c3d914fdec1073b6ed
AlexanderMattoni <amattoni@petrichor.io>
2014-09-16 11:37:05 -0400

    test

--- public_html/rand/captureafo.php
+++ public_html/rand/captureafo.php
@@ -5,7 +5,7 @@ $dir = '/home/amattoni/public_html/rand/images/';
 rmdir($dir);
 mkdir($dir);
 chdir($dir);
-exec('pageres https://afo.com w3counter ', $output, $response);
+exec('pageres [ https://afo.com 1280x1024 ] ', $output, $response);
 
 print_r($output);
 
