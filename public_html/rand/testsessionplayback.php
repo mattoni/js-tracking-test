80ddc61205270eb1693b88d79946638f9135c1a7
AlexanderMattoni <amattoni@petrichor.io>
2014-09-16 16:18:59 -0400

    test jquery parse object

--- public_html/rand/testsessionplayback.php
+++ public_html/rand/testsessionplayback.php
@@ -1,9 +1,17 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+</head>
+<body>
+	<iframe src="http://alexmattoni.com/rand/getmousecoords.php" width="80%" height="600" id='frameDemo'></iframe>
+	<script>
+		var data = jQuery.parseJSON('{"window":{"height":{"inner":592,"outer":1040},"width":{"inner":1920,"outer":1920}},"screen":{"height":{"available":1040,"total":1080},"width":{"available":1920,"total":1920},"depth":{"color":24,"pixel":24}},"client":{"agent":"Mozilla\/5.0 (Windows NT 6.1; WOW64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/37.0.2062.120 Safari\/537.36","language":"en-US","cookies":{"enabled":true},"url":"http:\/\/alexmattoni.com\/rand\/getmousecoords.php","ip":"173.236.68.4"},"timeline":{"clicks":[{"x":153,"y":75,"path":"body","time":1410898151},{"x":167,"y":213,"path":"body","time":1410898151},{"x":63,"y":56,"path":"body div.myclass div.hi","time":1410898152},{"x":41,"y":17,"path":"body span","time":1410898153},{"x":23,"y":32,"path":"body div.myclass div.nested","time":1410898154}],"movements":[{"x":63,"y":56,"path":"body div.myclass div.hi","time":1410898152},{"x":40,"y":17,"path":"body span","time":1410898153},{"x":23,"y":32,"path":"body div.myclass div.nested","time":1410898154},{"x":108,"y":15,"path":"body span","time":1410898155}],"scrolls":[],"resizes":[]}}')
 
-<script>
-	var data = jQuery.parseJSON('{"window":{"height":{"inner":592,"outer":1040},"width":{"inner":1920,"outer":1920}},"screen":{"height":{"available":1040,"total":1080},"width":{"available":1920,"total":1920},"depth":{"color":24,"pixel":24}},"client":{"agent":"Mozilla\/5.0 (Windows NT 6.1; WOW64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/37.0.2062.120 Safari\/537.36","language":"en-US","cookies":{"enabled":true},"url":"http:\/\/alexmattoni.com\/rand\/getmousecoords.php","ip":"173.236.68.4"},"timeline":{"clicks":[{"x":153,"y":75,"path":"body","time":1410898151},{"x":167,"y":213,"path":"body","time":1410898151},{"x":63,"y":56,"path":"body div.myclass div.hi","time":1410898152},{"x":41,"y":17,"path":"body span","time":1410898153},{"x":23,"y":32,"path":"body div.myclass div.nested","time":1410898154}],"movements":[{"x":63,"y":56,"path":"body div.myclass div.hi","time":1410898152},{"x":40,"y":17,"path":"body span","time":1410898153},{"x":23,"y":32,"path":"body div.myclass div.nested","time":1410898154},{"x":108,"y":15,"path":"body span","time":1410898155}],"scrolls":[],"resizes":[]}}')
+		$.each(data.timeline.clicks, function(index, element) {
+			$("#frameDemo").contents().find(element.path).css("background-color","#BADA55");
+		});
+	</script>
 
-	$.each(data.timeline.clicks, function(index, element) {
-		alert(element.time);
-	});
-</script>
+</body>
+</html>
