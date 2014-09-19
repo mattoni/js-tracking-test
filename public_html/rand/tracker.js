dbb65b8af029d02bbc1936872928104ea5abea02
AlexanderMattoni <amattoni@petrichor.io>
2014-09-18 23:33:58 -0400

    fancy dom selector

--- public_html/rand/tracker.js
+++ public_html/rand/tracker.js
@@ -278,49 +278,6 @@ function getDomStructure(event) {
 	return rightArrowParents.join(" ");
 }
 
-$.fn.extend({
-	/**
-	 * Return a valid jQuery selector path to an object from a
-	 container
-	 * root - specify a root element from which to generate the path
-	 *
-	 * If no root is specified, then path is given from the document
-	 root
-	 * If root is same as element, no path is returned (undefined)
-	 * If root is not actually a parent of this element, then no path
-	 is
-	 * returned (undefined);
-	 *
-	 */
-	path : function() {
-		var r;
-		if(root) {
-			r = root[0];
-		} else {
-			r = $()[0];
-		}
-		var el = this[0];
-		if(el) {
-			var path = "";
-			while(el && el.parentNode && el != el.parentNode) {
-				if(el.nodeType == 9) {
-					// reached document node, no root provided.
-					return;
-				}
-				if (el.id){
-					path = el.tagName.toLowerCase()+"#"+el.id+""+path;
-					break;
-				} else {
-					path = el.tagName.toLowerCase()+":eq("+
-					$(el).prevAll(el.tagName).size()+") "+path;
-				}
-				var el = el.parentNode;
-			}
-		}
-		return path;
-	}
-});
-
 function generateId(size) {
 	var text = "";
 	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
