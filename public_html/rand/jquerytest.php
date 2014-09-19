97829ba27e724658b0be7b13f95d0a24c76b6c65
AlexanderMattoni <amattoni@petrichor.io>
2014-09-18 23:55:35 -0400

    test

--- public_html/rand/jquerytest.php
+++ public_html/rand/jquerytest.php
@@ -4,19 +4,7 @@
 	<meta charset="utf-8">
 	<title>eq demo</title>
 	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-	<script>
-		// Applies yellow background color to a single <li>
-		$( "ul.nav li:eq(1)" ).css( "backgroundColor", "#ff0" );
 
-		// Applies italics to text of the second <li> within each <ul class="nav">
-		$( "ul.nav" ).each(function( index ) {
-			$( this ).find( "li:eq(1)" ).css( "fontStyle", "italic" );
-		});
-
-		// Applies red text color to descendants of <ul class="nav">
-		// for each <li> that is the second child of its parent
-		$( "ul:eq(1)" ).css( "color", "red" );
-	</script>
 </head>
 <body>
 
@@ -30,6 +18,20 @@
 		<li>List 2, item 2</li>
 		<li>List 2, item 3</li>
 	</ul>
+
+	<script>
+		// Applies yellow background color to a single <li>
+		$( "ul.nav li:eq(1)" ).css( "backgroundColor", "#ff0" );
+
+		// Applies italics to text of the second <li> within each <ul class="nav">
+		$( "ul.nav" ).each(function( index ) {
+			$( this ).find( "li:eq(1)" ).css( "fontStyle", "italic" );
+		});
+
+		// Applies red text color to descendants of <ul class="nav">
+		// for each <li> that is the second child of its parent
+		$( "body:eq(2) ul:eq(1) li:eq(2)" ).css( "color", "red" );
+	</script>
 
 </body>
 </html>
