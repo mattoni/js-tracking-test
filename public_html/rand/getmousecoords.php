a41990a00f163b9625b22bd0c23cf655afee9805
AlexanderMattoni <amattoni@petrichor.io>
2014-09-29 19:38:30 -0400

    try insert script

--- public_html/rand/getmousecoords.php
+++ public_html/rand/getmousecoords.php
@@ -9,7 +9,7 @@
 	<link href="resources/css/global/default.css" type="text/css" rel="stylesheet" />
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
 	<link href='//fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
-	<script async src='//www.web.dev.concurra.com/tracker/tracker.js'></script>
+	<script async src='//web.dev.concurra.com/tracker/tracker.js'></script>
 	<script>
 		__concurra().setToken('hoEndE7iLa');
 		__concurra().init();
